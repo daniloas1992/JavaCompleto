487e8a5cf77917d8b93e4eb583bbdaac8cf136c2
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-12-06 21:26:09 -0300

    385. FlowPane

--- javaFX/src/layout/AppLayoutBorderPane.java
+++ javaFX/src/layout/AppLayoutBorderPane.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class AppLayoutABorderPane extends Application {
+public class AppLayoutBorderPane extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
---
+++ javaFX/src/layout/AppLayoutFlowPane.java
@@ -0,0 +1,24 @@
+package layout;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppLayoutFlowPane extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	
+		
+		Scene principal = new Scene(new TesteFlowPane(), 800, 600);
+		
+		primaryStage.setScene(principal);
+		primaryStage.setTitle("Gerenciador de Layout BorderPane");
+		primaryStage.show();
+		
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+}

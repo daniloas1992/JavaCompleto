5d6ae916a0ee0e18def65d3dc5c84658f4e5d7e1
Danilo A. Santos <danilo_as1992@hotmail.com>
2023-12-06 21:01:01 -0300

    383. AnchorPane

---
+++ javaFX/src/layout/Quadrado.java
@@ -0,0 +1,28 @@
+package layout;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Quadrado extends Rectangle{
+
+private static int i = 0;
+	
+	private String[] cores = {
+		"#c33c5e", "#39aac5", "#28d78a", "fb520c", "#6547a5", "f50428"	
+	};
+	
+	public Quadrado() {
+		this(100, 100);
+	}
+	
+	public Quadrado(int largura, int altura) {
+		
+		setWidth(largura);
+		setHeight(altura);
+		
+		setFill(Color.web(cores[i++]));
+				
+		if(i == 6) i =0;
+	}
+	
+}
--- javaFX/src/module-info.java
+++ javaFX/src/module-info.java
@@ -1,6 +1,10 @@
 //Project -> Propertes -> Java Build Path -> Libraries -> Add Lbrary -> JRE System Library -> Execution environment -> JavaSE-11(jre).
 module exerciciosfx {
+	
 	requires javafx.controls;
 	requires javafx.graphics;
+	
 	opens basico;
+	opens layout;
+	
 }

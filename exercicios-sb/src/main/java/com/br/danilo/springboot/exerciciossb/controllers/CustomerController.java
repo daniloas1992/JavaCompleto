6b753e7b476748d6e1bec642a26ba4bd0f0e4beb
Danilo <Danilo@DESKTOP-MR1FHP5>
2024-02-18 19:12:54 -0300

    RequestParam

--- exercicios-sb/src/main/java/com/br/danilo/springboot/exerciciossb/controllers/CustomerController.java
+++ exercicios-sb/src/main/java/com/br/danilo/springboot/exerciciossb/controllers/CustomerController.java
@@ -1,7 +1,9 @@
 package com.br.danilo.springboot.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.danilo.springboot.exerciciossb.models.Customer;
@@ -14,6 +16,16 @@ public class CustomerController {
 	public Customer getCustomer() {
 		return new Customer(28, "John", "123.456.789-01");
 		
+	}
+	
+	@GetMapping("/{id}") // Na URL fica: http://localhost:8080/customers/123
+	public Customer getCustomerById(@PathVariable int id) {
+		return new Customer(id, "Jhonnas", "123.45.789-02");
+	}
+	
+	@GetMapping  // Na URL fica: http://localhost:8080/customers?id=123
+	public Customer getCustomerById2(@RequestParam(name = "id") int id) {
+		return new Customer(id, "Augustus", "123.45.789-03");
 	}
 
 }

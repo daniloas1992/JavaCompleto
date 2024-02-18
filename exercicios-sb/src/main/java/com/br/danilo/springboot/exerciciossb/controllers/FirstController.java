71fd76c6466b973d0122f1693186116368f1dc0d
Danilo <Danilo@DESKTOP-MR1FHP5>
2024-02-18 15:44:59 -0300

    Update

--- exercicios-sb/src/main/java/com/br/danilo/springboot/exerciciossb/controllers/FirstController.java
+++ exercicios-sb/src/main/java/com/br/danilo/springboot/exerciciossb/controllers/FirstController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PrimeiroController {
+public class FirstController {
 	
 	//@RequestMapping(method = RequestMethod.GET, path = "/ola")
 	//@GetMapping(path = "/ola")

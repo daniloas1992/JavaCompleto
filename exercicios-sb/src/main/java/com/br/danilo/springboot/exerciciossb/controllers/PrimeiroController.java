71188881b5e45ae878218dc75ca33d14b901c619
Danilo <Danilo@DESKTOP-MR1FHP5>
2024-02-18 15:37:01 -0300

    Start Spring Boot

---
+++ exercicios-sb/src/main/java/com/br/danilo/springboot/exerciciossb/controllers/PrimeiroController.java
@@ -0,0 +1,18 @@
+package com.br.danilo.springboot.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+	
+	//@RequestMapping(method = RequestMethod.GET, path = "/ola")
+	//@GetMapping(path = "/ola")
+	@GetMapping(path = {"/ola", "/saudacao"}) // Mais de uma URL para para o mesmo método
+	public String ola() {
+		return "Olá!";
+	}
+
+}
---
+++ exercicios-sb/src/test/java/com/br/danilo/springboot/exerciciossb/ExerciciosSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.danilo.springboot.exerciciossb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

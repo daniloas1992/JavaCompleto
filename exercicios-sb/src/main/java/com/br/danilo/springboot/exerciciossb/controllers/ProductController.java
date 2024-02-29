3dfc5fdf5d844c4c847431b747c12efe22aac5ed
Danilo <Danilo@DESKTOP-MR1FHP5>
2024-02-28 21:50:07 -0300

    CRUD

--- exercicios-sb/src/main/java/com/br/danilo/springboot/exerciciossb/controllers/ProductController.java
+++ exercicios-sb/src/main/java/com/br/danilo/springboot/exerciciossb/controllers/ProductController.java
@@ -1,14 +1,22 @@
 package com.br.danilo.springboot.exerciciossb.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.danilo.springboot.exerciciossb.model.entities.Product;
 import com.br.danilo.springboot.exerciciossb.model.repositories.ProductRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/products")
@@ -17,11 +25,44 @@ public class ProductController {
 	@Autowired
 	private ProductRepository productRepository;
 	
-	@PostMapping
+	/*@PostMapping
 	public @ResponseBody Product newProduct(@RequestParam String name, @RequestParam double price, @RequestParam double discount) {
 		Product product = new Product(name, price, discount);
 		productRepository.save(product);
 		return product;
+	}*/
+	
+	/*@PostMapping  // o método "newProduct" e "alterProduct" foram concentrados no método "saveProduct"
+	public @ResponseBody Product newProduct(@Valid Product product) {
+		productRepository.save(product);
+		return product;
+	}
+	
+	@PutMapping
+	public Product alterProduct(@Valid Product product) {
+		productRepository.save(product);
+		return product;
+	}*/
+	
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Product saveProduct(@Valid Product product) {
+		productRepository.save(product);
+		return product;
+	}
+	
+	@GetMapping
+	public Iterable<Product> getProducts() {
+		return productRepository.findAll();
+	}
+	
+	@GetMapping(path="/{id}")
+	public Optional<Product> getProductById(@PathVariable int id) {
+		return productRepository.findById(id);
+	}
+	
+	@DeleteMapping(path="/{id}")
+	public void deleteProduct(@PathVariable int id) {
+		productRepository.deleteById(id);
 	}
 
 }
--- exercicios-sb/src/main/java/com/br/danilo/springboot/exerciciossb/model/entities/Product.java
+++ exercicios-sb/src/main/java/com/br/danilo/springboot/exerciciossb/model/entities/Product.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Product {
@@ -23,10 +26,14 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@NotBlank
 	private String name;
 	
+	@Min(0)
 	private double price;
 	
+	@Min(0)
+	@Max(1)
 	private double discount;
 
 	public int getId() {
